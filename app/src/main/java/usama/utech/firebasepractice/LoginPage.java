4b5834a2cf8baa5c681e3851399fae70f676bef4
Syed Usama Ahmad <syedusama5556>
2019-03-17 21:48:17 +0500

    Fyp Project

--- app/src/main/java/usama/utech/firebasepractice/LoginPage.java
+++ app/src/main/java/usama/utech/firebasepractice/LoginPage.java
@@ -22,7 +22,7 @@ public class LoginPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+        setContentView(R.layout.activity_main);
 
 
         mAuth = FirebaseAuth.getInstance();
