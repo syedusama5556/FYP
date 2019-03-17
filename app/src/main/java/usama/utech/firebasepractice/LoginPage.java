87567c3db89028b439c537f2bf85a91ff449a6ed
Syed Usama Ahmad <syedusama5556>
2019-03-17 21:25:13 +0500

    Fyp Project

---
+++ app/src/main/java/usama/utech/firebasepractice/LoginPage.java
@@ -0,0 +1,87 @@
+package usama.utech.firebasepractice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginPage extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText email;
+    private EditText pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+//        if (mAuth.getInstance() != null){
+//            startActivity(new Intent(getApplicationContext(), Test.class));
+//        }
+
+        email = (EditText) findViewById(R.id.email);
+        pass = (EditText) findViewById(R.id.pass);
+
+        findViewById(R.id.loginBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signInWithEmailAndPassword(email.getText().toString(), pass.getText().toString())
+                        .addOnCompleteListener(LoginPage.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                                    FirebaseUser user = mAuth.getCurrentUser();
+
+                                    startActivity(new Intent(getApplicationContext(), Test.class));
+
+                                } else {
+
+                                    Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
+        });
+
+
+        findViewById(R.id.signup).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.createUserWithEmailAndPassword(email.getText().toString(), pass.getText().toString())
+                        .addOnCompleteListener(LoginPage.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+
+                                    Toast.makeText(getApplicationContext(), "Success",Toast.LENGTH_SHORT).show();
+
+
+                                } else {
+
+                                    Toast.makeText(getApplicationContext(), "Authentication failed.",Toast.LENGTH_SHORT).show();
+
+                                }
+
+                                // ...
+                            }
+                        });
+
+
+            }
+        });
+    }
+}
